3b37d8c8d021c7eba2c636609fd4b5c8b8f04db2
Princ84 <118486448+Princ84@users.noreply.github.com>
2023-04-17 13:37:52 +0300

    all update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const bodyColor = document.querySelector('body');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+
+let timeID = null;
+
+startBtn.addEventListener('click', () => {
+  timeID = setInterval(() => {
+    bodyColor.style.background = getRandomHexColor();
+    startBtn.disabled = true;
+  }, 1000);
+  stopBtn.disabled = false;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(timeID);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
